4f402153035ebb7f375c75770fe23109c023bb38
marta-0 <kaczmarskamarta2@gmail.com>
2023-05-05 06:50:34 +0200

    replace nb with bash

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,3 +1,4 @@
 set -e
 docker build -t vesuvius .
-docker run --gpus all --hostname $(hostname) --ipc=host --rm -v $(pwd):/host -v /mnt/12TB/Data/Kaggle/Vesuvius2023:/data -p 18888:18888 -it vesuvius jupyter notebook --allow-root --ip 0.0.0.0 --port 18888
+# docker run --gpus all --hostname $(hostname) --ipc=host --rm -v $(pwd):/host -v /mnt/12TB/Data/Kaggle/Vesuvius2023:/data -p 18003:18003 -it vesuvius jupyter notebook --allow-root --ip 0.0.0.0 --port 18003
+docker run --gpus all --hostname $(hostname) --ipc=host --rm -v $(pwd):/host -v /mnt/ssd4/Data/Kaggle/Vesuvius2023:/data -it vesuvius bash
