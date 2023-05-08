79fcbae9a4cf90a1110e89ce98f551c2844ee4d8
Karol Majek <karolmajek@gmail.com>
2023-05-08 06:14:36 +0200

    u2net and xunet

--- train.sh
+++ train.sh
@@ -1,1 +1,10 @@
-python train.py params/unet.py
+python train.py params/xunet_pos_weight_lr1e-6.py
+#python train.py params/xunet_pos_weight_lr.py
+#python train.py params/u2net_pos_weight_lr.py
+#python train.py params/u2net_pos_weight.py
+
+#python train.py params/unet.py
+#python train.py params/unet_pos_weight.py
+#python train.py params/unet_pos_weight_lr.py
+#python train.py params/unet_pos_weight_lr_small.py
+#python train.py params/unet_pos_weight5.py
